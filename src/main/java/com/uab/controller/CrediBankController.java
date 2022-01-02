4eda2f5aa8cf0790fbc86141bb4b49d6a1516989
Luis Mattoso <lmattoso84@gmail.com>
2022-01-02 16:35:45 +0000

    Ajuste no nome do serviço CreditBankService

--- src/main/java/com/uab/controller/CrediBankController.java
+++ src/main/java/com/uab/controller/CrediBankController.java
@@ -1,7 +1,7 @@
 package com.uab.controller;
 
 import com.uab.model.credibank.CheckResponseDTO;
-import com.uab.service.CrediBankService;
+import com.uab.service.CreditBankService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class CrediBankController {
 
     @Autowired
-    private CrediBankService crediBankService;
+    private CreditBankService creditBankService;
 
     @GetMapping(value = "check/{credit_account_id}/amount/{value}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public CheckResponseDTO getCheck(@PathVariable(name = "credit_account_id") String creditAccountId,
                                      @PathVariable(name = "credit_account_id") Integer value) {
-        return this.crediBankService.check(creditAccountId, value);
+        return this.creditBankService.check(creditAccountId, value);
     }
 }
--- src/main/java/com/uab/service/CreditBankService.java
+++ src/main/java/com/uab/service/CreditBankService.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(value = "creditbank", url = "http://ken.utad.pt:8181")
-public interface CrediBankService {
+public interface CreditBankService {
 
     @GetMapping(value = "check/{credit_account_id}/amount/{value}", produces = MediaType.APPLICATION_JSON_VALUE)
     CheckResponseDTO check(@PathVariable(name = "credit_account_id") String creditAccountId,
                            @PathVariable(name = "value") Integer value);
 }
+
+
